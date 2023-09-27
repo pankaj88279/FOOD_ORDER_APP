80021821c49d84727262b030254aafbbb23bd2e1
pankaj88279 <pankajjawi1993@gmail.com>
2023-09-27 13:02:15 +0530

    okkk

--- src/middlewere/authmiddilewere.js
+++ src/middlewere/authmiddilewere.js
@@ -3,19 +3,17 @@ let express = require('express');
 const jwt = require('jsonwebtoken');
 
 const { register } = require('../model/user')
-
+/* 
 let authMiddleware = (req, res, next) => {
 
     if (req.headers.authorization === undefined) {
 
         res.status(200).json({ msg: 'Unauthorized' })
     }
+       let token = req.headers.authorization.split(' ')[1];
+    
     
     //  console.log("data--->",req.headers.authorization.split(" ")[1]);
-
-    let token = req.headers.authorization.split(' ')[1];
-    
-
     try {
         //   console.log("decode---->",token)
         var decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -30,6 +28,25 @@ let authMiddleware = (req, res, next) => {
         });
     }
     
+}; */
+
+let authMiddleware = (req, res, next) => {
+    if (req.headers.authorization === undefined) {
+        res.status(401).json({ msg: 'Unauthorized' });
+    } else {
+        let token = req.headers.authorization.split(' ')[1];
+
+        try {
+            var decoded = jwt.verify(token, process.env.JWT_SECRET);
+            console.log("decode---->", decoded);
+            req.user = decoded;
+            next();
+        } catch (error) {
+            res.status(403).json({
+                msg: 'Unauthorized',
+            });
+        }
+    }
 };
 
 let enduserAuth = (req, res, next) => {
